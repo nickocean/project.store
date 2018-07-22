ac8ca3d5ec04e6800be20dcc3b6679f4e1751c4d
nickocean <39597663+nickocean@users.noreply.github.com>
2018-07-22 23:46:05 +0300

    Update product.php

--- app/views/product.php
+++ app/views/product.php
@@ -12,22 +12,14 @@
 
                     <!-- Nav tabs -->
                     <ul class="nav nav-tabs" role="tablist">
-
                         <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Details</a></li>
                         <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Comments</a></li>
-
-                        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab"
-                                                                  data-toggle="tab">Details</a></li>
-                        <li role="presentation"><a href="#messages" aria-controls="messages" role="tab"
-                                                   data-toggle="tab">Comments</a></li>
-
                     </ul>
 
                     <!-- Tab panes -->
                     <div class="tab-content">
                         <div role="tabpanel" class="tab-pane active" id="home"
                              style="margin-top: 20px"><?= $this->params['content'][0]['description'] ?></div>
-
                         <p><a href="/basket?id=<?= $this->params['content'][0]['id'] ?>" class="btn btn-primary" role="button" style="margin-top: 20px">Add to Basket</a>
                             <a href="/" class="btn btn-default" role="button" style="margin-top: 20px">Go back</a></p>
                         <div role="tabpanel" class="tab-pane" id="messages" style="margin-top: 20px">
@@ -54,20 +46,10 @@
                                     </div>
                                 </div>
 							<? endif; ?>
-
-                        <div role="tabpanel" class="tab-pane" id="messages" style="margin-top: 20px">
-                            <div>Name</div>
-                            <hr>
-                            <div><?= $this->params['comments'][0]['text'] ?></div>
-
                         </div>
                     </div>
 
                 </div>
-
-
-                <p><a href="/basket?id=<?= $this->params['content'][0]['id'] ?>" class="btn btn-primary" role="button" style="margin-top: 20px">Add to Basket</a> <a href="/" class="btn btn-default" role="button" style="margin-top: 20px">Go back</a></p>
-
             </div>
         </div>
     </div>
