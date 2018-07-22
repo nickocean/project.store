3215859fcdb7d04baeb220e1e6fe193d1859a741
nickocean <39597663+nickocean@users.noreply.github.com>
2018-07-22 23:47:28 +0300

    Update Product.php

--- app/models/Product.php
+++ app/models/Product.php
@@ -18,25 +18,15 @@ class Product extends Model
     public function getComments($id)
     {
         $comments = $this->db->row("SELECT * FROM comments WHERE product_id = $id");
-
         foreach ($comments as $key => $comment) {
         	$comments[$key]['user_name'] = $this->db->row("SELECT name FROM users WHERE id = {$comment['user_id']}");
         }
 
-
         return $comments;
     }
-
 
     public function addComment($id)
     {
     	$this->db->query("INSERT INTO comments (text, user_id, product_id) VALUES ('{$_POST['text']}', {$_SESSION['user'][0]['id']}, {$id})");
-
-    public function getUsers($id)
-    {
-        $comments = $this->db->row("SELECT * FROM comments WHERE product_id = $id");
-
-        return $comments;
-
     }
 }
