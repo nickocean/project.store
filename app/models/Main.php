145df53e4de407efac9a8a531a1971902fd76a7b
nickocean <nickocean1997@gmail.com>
2018-08-22 11:47:30 +0300

    Pagination updated

--- app/models/Main.php
+++ app/models/Main.php
@@ -9,15 +9,29 @@ class Main extends Model
 
     public function getData($page)
     {
-    	if (!isset($page) || $page == 1) {
-		    $result = $this->db->query("SELECT * FROM products WHERE id < 4;");
-	    } elseif ($page == 2) {
-		    $result = $this->db->query("SELECT * FROM products WHERE id > 3 AND id < 7;");
-	    } elseif ($page == 3) {
-		    $result = $this->db->query("SELECT * FROM products WHERE id > 6 AND id < 10;");
-	    } elseif ($page == 4) {
-		    $result = $this->db->query("SELECT * FROM products WHERE id > 9 AND id < 13;");
+    	$min = 0;
+    	$max = 4;
+
+    	switch ($page) {
+		    case 1:
+		    	$min = 0;
+		    	$max = 4;
+		    	break;
+		    case 2:
+		    	$min = 3;
+		    	$max = 7;
+		    	break;
+		    case 3:
+			    $min = 6;
+			    $max = 10;
+			    break;
+		    case 4:
+			    $min = 9;
+			    $max = 13;
+			    break;
 	    }
+
+	    $result = $this->db->query("SELECT * FROM products WHERE id > '{$min}' AND id < '{$max}';");
 	    return $result;
     }
 }
